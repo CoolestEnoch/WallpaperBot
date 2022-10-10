ff0c27b8bb97b0dc1dd668a22365755c214490a1
CoolestEnoch <77060301+CoolestEnoch@users.noreply.github.com>
2022-10-10 22:59:42 +0800

    2.71

--- code/app/src/main/java/com/coolest/wallpaper/bot/utils/ReleaseNotesUtil.kt
+++ code/app/src/main/java/com/coolest/wallpaper/bot/utils/ReleaseNotesUtil.kt
@@ -18,66 +18,76 @@ object ReleaseNotesUtil {
     fun showAlertDialog(title: String?, str: String?, context: Context?) {
         builder = AlertDialog.Builder(context!!)
         alert = builder!!
-                .setTitle(title)
-                .setMessage(str)
-                .setPositiveButton("确定") { dialog, which -> }.create() //创建AlertDialog对象
+            .setTitle(title)
+            .setMessage(str)
+            .setPositiveButton("确定") { dialog, which -> }.create() //创建AlertDialog对象
         alert!!.show() //显示对话框
     }
 
     fun setReleaseNotes() {
         noteList.apply {
             add(
-                    Version(
-                            2.23,
-                            "[修复]Android9及以上系统的设备概率性刷不出来图片" + tab_and_new_line +
-                                    "[修复]刷新交互逻辑错误" + tab_and_new_line +
-                                    "[修复]部分图标显示问题" + tab_and_new_line +
-                                    "[优化]设置壁纸的速度" + tab_and_new_line +
-                                    "[优化]适配MIUI小白条沉浸" + tab_and_new_line +
-                                    "[优化]根据最小权限原则, 去除在Android11及以上设备上多申请的一个权限" + tab_and_new_line +
-                                    "[优化]全透明的浏览体验" + tab_and_new_line +
-                                    "[新增]图片加载动画" + tab_and_new_line +
-                                    "[新增]按钮高斯模糊"
-                    )
+                Version(
+                    2.23,
+                    "[修复]Android9及以上系统的设备概率性刷不出来图片" + tab_and_new_line +
+                            "[修复]刷新交互逻辑错误" + tab_and_new_line +
+                            "[修复]部分图标显示问题" + tab_and_new_line +
+                            "[优化]设置壁纸的速度" + tab_and_new_line +
+                            "[优化]适配MIUI小白条沉浸" + tab_and_new_line +
+                            "[优化]根据最小权限原则, 去除在Android11及以上设备上多申请的一个权限" + tab_and_new_line +
+                            "[优化]全透明的浏览体验" + tab_and_new_line +
+                            "[新增]图片加载动画" + tab_and_new_line +
+                            "[新增]按钮高斯模糊"
+                )
             )
             add(
-                    Version(
-                            2.5,
-                            "[新增]两个壁纸更新渠道" + tab_and_new_line +
-                                    "[修复]部分设备上图片保存失败导致的闪退" + tab_and_new_line +
-                                    "[修复]部分场景下深色模式显示异常"
-                    )
+                Version(
+                    2.5,
+                    "[新增]两个壁纸更新渠道" + tab_and_new_line +
+                            "[修复]部分设备上图片保存失败导致的闪退" + tab_and_new_line +
+                            "[修复]部分场景下深色模式显示异常"
+                )
             )
             add(
-                    Version(
-                            2.51,
-                            "[修复]部分设备上保存图片闪退的问题" + tab_and_new_line +
-                                    "[修复]转屏后会自动获取新图片" + tab_and_new_line +
-                                    "[修复]转屏后程序重进" + tab_and_new_line +
-                                    "[优化]图片保存逻辑" + tab_and_new_line +
-                                    "[优化]部分场景的性能和稳定性"
-                    )
+                Version(
+                    2.51,
+                    "[修复]部分设备上保存图片闪退的问题" + tab_and_new_line +
+                            "[修复]转屏后会自动获取新图片" + tab_and_new_line +
+                            "[修复]转屏后程序重进" + tab_and_new_line +
+                            "[优化]图片保存逻辑" + tab_and_new_line +
+                            "[优化]部分场景的性能和稳定性"
+                )
             )
             add(
-                    Version(
-                            2.6,
-                            "[适配]Material You 动态配色算法\n" +
-                                    "(需更新手机系统至Android 12或更高版本)"
-                    )
+                Version(
+                    2.6,
+                    "[适配]Material You 动态配色算法\n" +
+                            "(需更新手机系统至Android 12或更高版本)"
+                )
             )
             add(
-                    Version(
-                            2.61,
-                            "[修复]FydeOS 14.2(83)上保存图片失败的问题"
-                    )
+                Version(
+                    2.61,
+                    "[修复]FydeOS 14.2(83)上保存图片失败的问题"
+                )
             )
             add(
-                    Version(
-                            2.7,
-                            "[新增]有新图标啦, 不再是AndroidStudio默认的了" + tab_and_new_line +
-                                    "[新增]可单独设置锁屏or主屏壁纸" + tab_and_new_line +
-                                    "[新增]身边好友扫一扫分享壁纸"
-                    )
+                Version(
+                    2.7,
+                    "[新增]有新图标啦, 不再是AndroidStudio默认的了" + tab_and_new_line +
+                            "[新增]可单独设置锁屏or主屏壁纸" + tab_and_new_line +
+                            "[新增]身边好友扫一扫分享壁纸"
+                )
+            )
+            add(
+                Version(
+                    2.71,
+                    "[修复]部分不支持分享的图片将不显示分享链接和二维码按钮" + tab_and_new_line +
+//                            "[新增]非持久化历史记录" + tab_and_new_line +// TODO 非持久化历史记录
+                            "[修复]A屏深色模式下背景可能发绿的问题" + tab_and_new_line +
+                            "[新增]新的壁纸渠道" + tab_and_new_line +
+                            "[新增]图片放大等手势操作"
+                )
             )
         }
         Collections.reverse(noteList)
@@ -98,8 +108,8 @@ object ReleaseNotesUtil {
         releaseNotes
         val linearLayout = LinearLayout(context)
         val params = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         linearLayout.layoutParams = params
         linearLayout.orientation = LinearLayout.VERTICAL
@@ -113,34 +123,34 @@ object ReleaseNotesUtil {
                 }
             }
             linearLayout.addView(
-                    ViewUtils_Kotlin.createBigButton(
-                            context,
-                            R.drawable.ic_baseline_grass_24,
-                            R.color.item_card_bg,
-                            list,
-                            null,
-                            R.color.white
-                    ) {}
+                ViewUtils_Kotlin.createBigButton(
+                    context,
+                    R.drawable.ic_baseline_grass_24,
+                    R.color.item_card_bg,
+                    list,
+                    null,
+                    R.color.white
+                ) {}
             )
             Log.e("for", "run in")
         }
         Log.e("for", "after")
         val scrollView = ScrollView(context)
         val scrollParams: ViewGroup.LayoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         scrollView.layoutParams = scrollParams
         scrollView.addView(linearLayout)
         ViewUtils_Kotlin.getBigCardFromBottom(
-                context,
-                "更新日志\t当前版本${
-                    context.packageManager.getPackageInfo(
-                            context.packageName,
-                            0
-                    ).versionName
-                }",
-                scrollView
+            context,
+            "更新日志\t当前版本${
+                context.packageManager.getPackageInfo(
+                    context.packageName,
+                    0
+                ).versionName
+            }",
+            scrollView
         ).show()
     }
 
@@ -149,8 +159,8 @@ object ReleaseNotesUtil {
         releaseNotes
         val linearLayout = LinearLayout(context)
         val params = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         linearLayout.layoutParams = params
         linearLayout.orientation = LinearLayout.VERTICAL
@@ -162,19 +172,19 @@ object ReleaseNotesUtil {
             list.add(str)
         }
         linearLayout.addView(
-                ViewUtils_Kotlin.createBigButton(
-                        context,
-                        R.drawable.ic_baseline_grass_24,
-                        R.color.item_card_bg,
-                        list,
-                        null,
-                        R.color.white
-                ) {}
+            ViewUtils_Kotlin.createBigButton(
+                context,
+                R.drawable.ic_baseline_grass_24,
+                R.color.item_card_bg,
+                list,
+                null,
+                R.color.white
+            ) {}
         )
         val scrollView = ScrollView(context)
         val scrollParams: ViewGroup.LayoutParams = LinearLayout.LayoutParams(
-                LinearLayout.LayoutParams.MATCH_PARENT,
-                LinearLayout.LayoutParams.WRAP_CONTENT
+            LinearLayout.LayoutParams.MATCH_PARENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
         )
         scrollView.layoutParams = scrollParams
         scrollView.addView(linearLayout)
